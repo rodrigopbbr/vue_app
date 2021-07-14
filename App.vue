2d43c8c71b3f87c9bf3555be6ff4f84649d14cc9
Rodrigo Pereira <rodrigopbbr@gmail.com>
2021-07-13 22:19:49 -0300

    Primeira versão

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Clique Aqui" @press="exclaim" />
+    
+    
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: green;
+  font-size: 30;
+}
+</style>
